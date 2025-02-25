657485881d9f5dcdcc69161c0359e538b70060a1
Thara Gopinath <tgopinath@microsoft.com>
2025-02-25 19:53:13 +0000

    arch: x86: hyperv: hv_vtl: Add api to enable VTL1 for secondary cpus
    Introduce an api to enable VTL1 (secure vtl) for secondary cpus
    piggybacking on the earlier consolidated logic to enable VTL1.
    
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -319,9 +319,11 @@ static inline int hv_apicid_to_vp_index(u32 apic_id) { return -EINVAL; }
 #ifdef CONFIG_HYPERV_VTL_MODE
 void __init hv_vtl_init_platform(void);
 int __init hv_vtl_early_init(u8 vtl);
+int hv_secure_vtl_enable_secondary_cpu(u32 target_vp_index);
 #else
 static inline void __init hv_vtl_init_platform(void) {}
 static inline int __init hv_vtl_early_init(u8 vtl) { return 0; }
+static inline int hv_secure_vtl_enable_secondary_cpu(u32 target_vp_index) { return 0; }
 #endif
 
 #include <asm-generic/mshyperv.h>
