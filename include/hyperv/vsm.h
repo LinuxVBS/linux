874e92f584b5661b6d2af8a565049d2ee32aceff
Thara Gopinath <tgopinath@microsoft.com>
2025-05-30 08:37:23 -0400

    drivers: hv: hv_vsm_boot: Load secure kernel
    LVBS involves enabling VTL1 and then loading secure kernel in VTL1 from
    VTL0.  Add changes in the vsm boot driver to pick up secure kernel from
    initramfs and load it into a reserved memory area.
    
    Signed-off-by: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

---
+++ include/hyperv/vsm.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Common definition for enabling VTL1 and VSM.
+ *
+ * Copyright (c) 2025, Microsoft Corporation.
+ *
+ * Author:
+ *
+ */
+
+#ifndef _VSM_H
+#define _VSM_H
+
+#include <linux/types.h>
+#include <linux/align.h>
+
+/* Define PAGE size and related variables for initial secure kernel pages */
+#define VSM_PAGE_SHIFT			12
+#define VSM_PAGE_SIZE			(((uint32_t)1) << VSM_PAGE_SHIFT)
+#define PAGE_AT(addr, idx)		((addr) + (idx) * VSM_PAGE_SIZE)
+#define VSM_VA_FROM_PA(pa)		(pa) // Assumes identity mapping in secure kernel
+
+/* Number of entries in a page table (all levels) */
+#define VSM_ENTRIES_PER_PT		512
+#define VSM_PMD_SIZE			(VSM_PAGE_SIZE * VSM_ENTRIES_PER_PT)
+
+/*
+ * Initial memory that will be mapped for secure kernel.
+ * Secure Kernel memory can be larger than this.
+ */
+#define VSM_SK_INITIAL_MAP_SIZE		(16 * 1024 * 1024)
+#define VSM_SK_PTE_PAGES_COUNT	(ALIGN(VSM_SK_INITIAL_MAP_SIZE, VSM_PMD_SIZE) / VSM_PMD_SIZE)
+
+/* VSM pages */
+enum {
+	VSM_GDT_PAGE,
+	VSM_TSS_PAGE,
+	VSM_PML4E_PAGE,
+	VSM_PDPE_PAGE,
+	VSM_PDE_PAGE,
+	VSM_PTE_PAGES,
+	/* PTE tables consume several pages */
+	VSM_KERNEL_STACK_PAGE = VSM_PTE_PAGES + VSM_SK_PTE_PAGES_COUNT,
+	/* Kernel boot pages */
+	VSM_BOOT_PARAMS_PAGE,
+	VSM_CMDLINE_PAGE,
+	VSM_PAGES_COUNT
+};
+
+#define VSM_PAGES_SIZE          (VSM_PAGES_COUNT << VSM_PAGE_SHIFT)
+
+#endif /* _VSM_H */
